487104e21eec58f1733d2477e62c03e1dc338f1a
Jared Jenkins <jared@playnomics.com>
2013-07-29 16:48:47 -0700

    Removing rarely used features from the testing UI. Adding standard frame usecases for messaging.

--- PlaynomicsTestApp/src/com/playnomics/PlaynomicsTestAppActivity.java
+++ PlaynomicsTestApp/src/com/playnomics/PlaynomicsTestAppActivity.java
@@ -15,6 +15,8 @@ import com.playnomics.playrm.PlaynomicsConstants.TransactionType;
 import com.playnomics.playrm.PlaynomicsConstants.UserInfoSex;
 import com.playnomics.playrm.PlaynomicsConstants.UserInfoSource;
 import com.playnomics.playrm.PlaynomicsConstants.UserInfoType;
+import com.playnomics.playrm.Frame;
+import com.playnomics.playrm.Messaging;
 import com.playnomics.playrm.PlaynomicsSession;
 
 public class PlaynomicsTestAppActivity extends Activity {
@@ -26,7 +28,7 @@ public class PlaynomicsTestAppActivity extends Activity {
 	
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		PlaynomicsSession.setTestMode(true);
+		PlaynomicsSession.setTestMode(false);
 		System.out.println("START: " + PlaynomicsSession.start(this, applicationId).toString());
 	}
 	
@@ -41,30 +43,11 @@ public class PlaynomicsTestAppActivity extends Activity {
 		PlaynomicsSession.stop();
 		super.onDestroy();
 	}
-	
-	public void onChangeUserClick(View view) {
-		System.out.println("CHANGE USER: " +PlaynomicsSession.changeUser("testChangeUserId").toString());
-	}
+
 	
 	public void onUserInfoClick(View view) {
 		System.out.println("USER INFO: " +PlaynomicsSession.userInfo(UserInfoType.update, "USA", "test", UserInfoSex.Male, new Date("1/1/1999"),
 				UserInfoSource.Other, "test", new Date()).toString());
-	}
-	
-	public void onSessionStartClick(View view) {
-		System.out.println("SESSION START: " + PlaynomicsSession.sessionStart(3L, "TEST_SITE").toString());
-	}
-	
-	public void onSessionEndClick(View view) {
-		System.out.println("SESSION END: " + PlaynomicsSession.sessionEnd(3L, "QUIT").toString());
-	}
-	
-	public void onGameStartClick(View view) {
-		System.out.println("GAME START: "+PlaynomicsSession.gameStart(3L, 3L, "TEST_SITE", "TEST_TYPE", "TEST_GAME").toString());
-	}
-	
-	public void onGameEndClick(View view) {
-		System.out.println("GAME END: " + PlaynomicsSession.gameEnd(3L, 3L, "LOSE").toString());
 	}
 	
 	public void onTransactionClick(View view) {
@@ -76,32 +59,37 @@ public class PlaynomicsTestAppActivity extends Activity {
 				PlaynomicsSession.transaction(1234567890L, "TEST_ITEM_ID", null, TransactionType.BuyItem, "TEST_USER_ID",
 						currencyTypes, currencyValues, currencyCategories).toString());
 	}
-	
-	public void onInvitationSentClick(View view) {
-		Log.d(logTag, "INVITATION SENT: "
-				+ PlaynomicsSession.invitationSent(3L, "TEST_RECIPIENT", "TEST_ADDRESS", "TEST_METHOD")
-				.toString());
-	}
-	
-	public void onInvitationResponseClick(View view) {
-		Log.d(logTag, "INVITATION RESPONSE: "
-				+ PlaynomicsSession.invitationResponse(3L, "TEST_RECIPIENT",ResponseType.accepted).toString());
-	}
-	
+
 	public void onMilestoneClick(View view) {
-		Log.d(logTag, "INVITATION RESPONSE: "
-				+ PlaynomicsSession.invitationResponse(3L, "TEST_RECIPIENT",ResponseType.accepted).toString());
-		
-		System.out.println("TEST_MILESTONE: "+ PlaynomicsSession.milestone(3L, "TEST_MILESTONE").toString());
+		System.out.println("TEST_MILESTONE: "+ PlaynomicsSession.milestone(1L, "CUSTOM1").toString());
+		System.out.println("TEST_MILESTONE: "+ PlaynomicsSession.milestone(2L, "CUSTOM2").toString());
+		System.out.println("TEST_MILESTONE: "+ PlaynomicsSession.milestone(3L, "CUSTOM3").toString());
+		System.out.println("TEST_MILESTONE: "+ PlaynomicsSession.milestone(4L, "CUSTOM4").toString());
+		System.out.println("TEST_MILESTONE: "+ PlaynomicsSession.milestone(5L, "CUSTOM5").toString());
+	}
+
+	public void onHttpClick(View view){
+		Frame frame = Messaging.initWithFrameID("44841d6a2bcec8c9", this);
+		frame.start();
 	}
 	
-	public void onSwitchActivityClick(View view) {
-		Intent myIntent = new Intent(this, PlaynomicsTestAppActivity2.class);
-		startActivity(myIntent);
+	public void onJsonClick(View view){
+		Frame frame = Messaging.initWithFrameID("a40893b36c6ddb32", this);
+		frame.start();
 	}
 	
-	public void onCloseClick(View view) {
-		finish();
+	public void onNullTargetClick(View view){
+		Frame frame = Messaging.initWithFrameID("67dbfcad37eccbf9", this);
+		frame.start();
 	}
 	
+	public void onNoAdsClick(View view){
+		Frame frame = Messaging.initWithFrameID("5bc049bb66ffc121", this);
+		frame.start();
+	}
+	
+	public void onPnxClick(View view){
+		Frame frame = Messaging.initWithFrameID("e45c59f627043701", this);
+		frame.start();
+	}
 }
