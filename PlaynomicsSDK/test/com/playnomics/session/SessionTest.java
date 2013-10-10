952ec8361b1bd42c0580d0ff0f5454e058ddb0c0
Jared Jenkins <jared@playnomics.com>
2013-10-10 16:44:29 -0700

    Add test stubs for Session

---
+++ PlaynomicsSDK/test/com/playnomics/session/SessionTest.java
@@ -0,0 +1,67 @@
+package com.playnomics.session;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class SessionTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	/**
+	 * Should queue one event, appStart
+	 */
+	public void testStartLapse(){
+		fail("Not implemented");
+	}
+	
+	@Test
+	/**
+	 * Should queue one event, appPage
+	 */
+	public void testStartNoLapse(){
+		fail("Not implemented");
+	}
+	
+	
+	@Test 
+	public void testMilestone(){
+		fail("Not implemented");
+	}
+	
+	@Test 
+	public void testMilestoneNoStart(){
+		fail("Not implemented");
+	}
+	
+
+	@Test
+	public void testTransaction(){
+		fail("Not implemented");
+	}
+	
+	@Test 
+	public void testTransactionNoStart(){
+		fail("Not implemented");
+	}
+	
+}
