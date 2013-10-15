c023d8c7114cea686cd8b375cf7e2b3b8b0b02e3
Jared Jenkins <jared@playnomics.com>
2013-10-15 12:55:06 -0700

    Add tests for the heart beat producer

---
+++ PlaynomicsSDK/test/com/playnomics/session/HeartBeatProducerTest.java
@@ -0,0 +1,50 @@
+package com.playnomics.session;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Mockito.verify;
+
+import com.playnomics.util.Config;
+
+public class HeartBeatProducerTest {
+
+	@Mock
+	private HeartBeatHandler handler;
+	
+	private HeartBeatProducer producer;
+	private int intervalSeconds;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		MockitoAnnotations.initMocks(this);
+		intervalSeconds = 1;
+		producer = new HeartBeatProducer(intervalSeconds);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testProducer() throws InterruptedException {
+		producer.start(handler);
+		Thread.sleep((long)(intervalSeconds * 1.5 * 1000));
+		producer.stop();
+		verify(handler, Mockito.atLeastOnce()).onHeartBeat(intervalSeconds);
+	}
+}
